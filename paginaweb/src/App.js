2311f57253cd29f800bb67ef70f7a9afa287ae20
Mama679 <ingmariomanotas@hotmail.com>
2023-07-11 11:37:36 -0500

    Agrego Ruta del Proyecto

--- paginaweb/src/App.js
+++ paginaweb/src/App.js
@@ -1,13 +1,31 @@
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+//Paginas del Sitio Web
+import Home from './components/home/Home';
+import Nosotros from './components/nosotros/Nosotros';
+import Productos from './components/productos/Productos';
+import Contactos from './components/contactos/Contactos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
-  );
+class App extends React.Component{ 
+  
+
+  render(){
+    
+    return(
+      <React.Fragment>
+          <Router>
+            <Routes>
+                <Route exact path='/' element={<Home/>} />                 
+                <Route exact path="/nosotros" element={<Nosotros />}/>                                
+                <Route exact path="/productos" element={<Productos />}/>
+                <Route exact path="/contactos" element={<Contactos />}/> 
+            </Routes>      
+          </Router>
+      </React.Fragment>
+    );
+  
+  }
 }
-
 export default App;
+
--- paginaweb/src/components/menu/Menu.js
+++ paginaweb/src/components/menu/Menu.js
@@ -1,26 +1,27 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 class Menu extends React.Component{
     render(){
         return(
             <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-                 <a className="navbar-brand" href="#">Mi Proyecto</a>
+                 <Link to="/" className="navbar-brand" >Mi Proyecto</Link>
                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                  </button>
                  <div className="collapse navbar-collapse" id="navbarCollapse">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            <Link to="/" className="nav-link">Home <span className="sr-only">(current)</span></Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Nosotros</a>
+                            <Link to="/nosotros" className="nav-link">Nosotros</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Servicios</a>
+                            <Link to="/productos" className="nav-link">Servicios</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Contacto</a>
+                            <Link to="/contactos" className="nav-link">Contacto</Link>
                         </li>
                     </ul>
                     <form className="form-inline mt-2 mt-md-0">
